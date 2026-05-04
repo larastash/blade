443360bbd4c324d4cecbcf1ea88e7bea86c00407
chipslays <gorshkov34@gmail.com>
2026-05-04 15:19:52 +0400

    1.0.3

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,8 @@ import Alpine from './alpine';
 import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import '../../vendor/masmerise/livewire-toaster/resources/js';
 
+import Autosize from '@marcreichel/alpine-autosize';
+
+Alpine.plugin(Autosize);
+
 Livewire.start();
