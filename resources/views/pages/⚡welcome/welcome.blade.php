de3febd5c89dcc5c366a8c6840ad0503a6515370
chipslays <gorshkov34@gmail.com>
2026-08-26 22:30:08 +0400

    13.0.4

--- resources/views/pages/⚡welcome/welcome.blade.php
+++ resources/views/pages/⚡welcome/welcome.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="absolute top-4 right-4 flex items-center gap-4">
-        <button x-data="theme" @click="toggleTheme" class="p-2 rounded-brand hover:bg-gray-200 dark:hover:bg-gray-800">
+        <button x-data="theme" @click="toggleTheme" class="p-2 rounded-brand hover:bg-300">
             <x-icon x-show="currentTheme === 'light'" x-cloak name="lucide-sun" class="size-5" />
             <x-icon x-show="currentTheme === 'dark'" x-cloak name="lucide-moon" class="size-5" />
             <x-icon x-show="currentTheme === 'system'" x-cloak name="lucide-sun-moon" class="size-5" />
@@ -23,7 +23,7 @@
     <div class="absolute bottom-4 inset-x-4">
         <div class="flex items-center justify-between">
             <div class="flex items-center gap-2.5">
-                <figure class="shrink-0 size-1.5 rounded-full bg-green-500 dark:bg-green-600 ring-2 ring-green-300 dark:ring-green-500 animate-pulse">
+                <figure class="shrink-0 size-1.5 rounded-full bg-success-500 dark:bg-success-600 ring-2 ring-success-300 dark:ring-success-500 animate-pulse">
                 </figure>
                 <p class="text-600 leading-none">
                     ready for something awesome
